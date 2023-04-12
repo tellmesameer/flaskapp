a05fe04d97be4448a723f2d11e1915dbdc01ec01
anshulatul <anshulkushal51@gmail.com>
2023-04-12 11:43:10 +0530

    xcxv

--- try.py
+++ try.py
@@ -1,3 +1,64 @@
-ster='select [OrderNumber],[Name],[Email],[GiftCycleId],[DeliveryURL],[ShippingDate] from [dbo].[Order] LEFT JOIN [dbo].[Recipient]ON [dbo].[Order].RecipientId = [dbo].[Recipient].RecipientId where OrderNumber is not null and [GiftCycleId]=?'
-sml=ster[:-19]
-print(sml)
+print("Welcome to the Love Calculator!")
+
+name1 = input("Enter your name: ").lower()
+
+name2 = input("Enter the name of the person you love: ").lower()
+
+
+
+
+combined_name = name1 + name2
+
+
+
+
+true_count = 0
+
+love_count = 0
+
+
+
+
+true_count += combined_name.count('t')
+
+true_count += combined_name.count('r')
+
+true_count += combined_name.count('u')
+
+true_count += combined_name.count('e')
+
+
+
+
+love_count += combined_name.count('l')
+
+love_count += combined_name.count('o')
+
+love_count += combined_name.count('v')
+
+love_count += combined_name.count('e')
+
+
+
+
+score = int(str(true_count) + str(love_count))
+
+
+
+
+if score < 10 or score > 90:
+
+    message = "Your score is {}, you go together like coke and mentos.".format(score)
+
+elif score >= 40 and score <= 50:
+
+    message = "Your score is {}, you are alright together.".format(score)
+
+else:
+
+    message = "Your score is {}.".format(score)
+
+
+
+
+print(message)
